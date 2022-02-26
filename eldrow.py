b4f9e841188fe582f1e35420ff6f5b49042a3f7d
Alex Szczuczko <alex@szc.ca>
2022-02-26 10:55:22 -0700

    Add game interpreter subcommand

--- eldrow.py
+++ eldrow.py
@@ -131,24 +131,110 @@ def suggest_words(wordlist: list[str], contains_letters: str, uncontained_letter
     return narrow
 
 #
+# Game Interpreter
+#
+
+def interperet_game_raw(raw_moves: list[str]) -> dict:
+    if len(raw_moves) > 0:
+        length = len(raw_moves[0])
+        assert all(len(m) == length for m in raw_moves), "Moves are not all the same length"
+        assert len(raw_moves) % 2 == 0, "Moves are not all paired with a response, odd number of total elements"
+        moves = list(m.lower() for m in raw_moves[::2])
+        responses = list(r.lower() for r in raw_moves[1::2])
+    else:
+        length = 5
+        moves = []
+        responses = []
+
+    return interperet_game(list(zip(moves, responses)))
+
+def interperet_game(moves_and_responses: list) -> dict:
+    if len(moves_and_responses) > 0:
+        length = len(moves_and_responses[0][0])
+    else:
+        length = 5
+    indexes = list(range(0, length))
+
+    contained = set()
+    uncontained = set()
+    known = ["."] * length
+    known_not = []
+
+    for move, response in moves_and_responses:
+        kn = ["."] * length
+
+        for m, r, i, in zip(move, response, indexes):
+            if r == "b":
+                # b: letter uncontained
+                uncontained.add(m)
+            elif r == "y":
+                # y: letter contained, but not at this position
+                contained.add(m)
+                kn[i] = m
+            elif r == "g":
+                # g: letter known
+                known[i] = m
+            else:
+                raise ValueError(f"Invalid response code {r}")
+
+        if not all(e == "." for e in kn):
+            known_not.append("".join(kn))
+
+    status = "open"
+    if moves_and_responses[-1][1] == "g" * length:
+        status = "won"
+    elif len(moves_and_responses) >= 6:
+        status = "lost"
+
+    return {
+        "present": "".join(sorted(contained)),
+        "not_present": "".join(sorted(uncontained)),
+        "known_positions": "".join(known),
+        "known_non_positions": known_not,
+        "status": status,
+    }
+
+#
 # Main
 #
 
 def arg_parser():
-    parser = argparse.ArgumentParser(
-        description="Given the state of a Wordle game, and a wordlist, suggest one or more possible solutions",
+    root_parser = argparse.ArgumentParser(
+        description="Tool for solving Wordle word puzzles",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
-    parser.add_argument("-o", "--output", help="Write the suggested word(s) to a file instead of stdout")
-    parser.add_argument("-l", "--limit", type=int, default=10, help="Change the default maximum number of suggested words")
-    parser.add_argument("-s", "--show-score", action="store_true", help="Show the coincidence score alongside each suggested word")
-    parser.add_argument("-w", "--wordlist", default="wordlist.txt", help="Change the default path of the wordlist file")
+    subparsers = root_parser.add_subparsers()
+
+    def addargs_su_ga(parser):
+        parser.add_argument("-o", "--output", help="Write the suggested word(s) to a file instead of stdout")
+        parser.add_argument("-l", "--limit", type=int, default=10, help="Change the default maximum number of suggested words")
+        parser.add_argument("-s", "--show-score", action="store_true", help="Show the coincidence score alongside each suggested word")
+        parser.add_argument("-w", "--wordlist", default="wordlist.txt", help="Change the default path of the wordlist file")
+
+    parser = subparsers.add_parser(
+        "suggest",
+        aliases=["su"],
+        help="Given the state of a Wordle game, and a wordlist, suggest one or more possible solutions",
+        description="Given the state of a Wordle game, and a wordlist, suggest one or more possible solutions",
+    )
+    parser.set_defaults(which="suggest")
+    addargs_su_ga(parser)
     parser.add_argument("-p", "--present", default="", help="Specify any letters that are known to exist somewhere in the word. Order doesn't matter.")
     parser.add_argument("-n", "--not-present", default="", help="Specify any letters that are known to not exist anywhere in the word. Any letters specified in the list of present letters will override letters specified here. Order doesn't matter.")
     parser.add_argument("-k", "--known-positions", default=".....", help="Specify any positions/columns that are known to contain a particular letter. Use a period character (or any other non-letter) to specify unknown positions. Any letters specified here will also be added to the list of present letters. Must be the same length as the words in the wordlist. Examples: a..ot .b... ....s")
     parser.add_argument("-i", "--known-non-positions", action="append", default=["....."], help="Specify any positions/columns that are known to NOT contain a particular letter. Multiple overlapping patterns can be entered via sequential uses of this argument. Same syntax as --known-positions")
 
-    return parser
+    parser = subparsers.add_parser(
+        "game",
+        aliases=["ga"],
+        help="Given a wordlist, and zero or more previous moves and responses, suggest one or more possible solutions",
+        description="Given a wordlist, and zero or more previous moves and responses, suggest one or more possible solutions",
+    )
+    parser.set_defaults(which="game")
+    addargs_su_ga(parser)
+    parser.add_argument("moves", nargs="*", default=[], metavar="MOVE RESPONSE", help="A move followed by the response from Wordle. There should be an even number of arguments. Use y for yellow, g for green, b for black. Ex: abort ygbbb")
+
+    return root_parser
 
 def main(raw_args):
     parser = arg_parser()
@@ -161,8 +247,19 @@ def main(raw_args):
     # Parse it
     wordlist, scores_by_word = read_wordlist(raw_wordlist)
 
-    # Generate all suggestions
-    suggestions = suggest_words(wordlist, args.present, args.not_present, args.known_positions, args.known_non_positions)
+    # Run appropriate function based on subcommand used
+
+    if args.which == "suggest":
+        suggestions = suggest_words(wordlist, args.present, args.not_present, args.known_positions, args.known_non_positions)
+    elif args.which == "game":
+        game = interperet_game_raw(args.moves)
+        if game["status"] != "open":
+            print(f"Game has been {game['status']}", flush=True, file=sys.stderr)
+            return 0
+        suggestions = suggest_words(wordlist, game["present"], game["not_present"], game["known_positions"], game["known_non_positions"])
+    else:
+        parser.print_usage()
+        return 1
 
     # Determine zero padding requirements
     if suggestions:
@@ -183,6 +280,8 @@ def main(raw_args):
             write(f)
     else:
         write(sys.stdout)
+
+    return 0
 
 if __name__ == "__main__":
     sys.exit(main(sys.argv[1:]))
